59e162d7789f49464f1754fff7f550058827b6a6
Rosa Mora <ramm86@gmail.com>
2021-04-02 09:32:56 -0400

    Codigo para cargar JQuery
    Codigo para cargar JQuery en momento específico que se necesita en Wordpress, en situaciones cuando el theme lo carga después y no se pueden ejecutar ciertas funciones.

---
+++ cargar-jquery-wp.js
@@ -0,0 +1,4 @@
+//Codigo para insertar JQuery en plantilla de Wordpress cuando no carga el JQuery en el momento necesitado
+//por la funcion, ya sea porque el theme lo carga en el footer o porque lo minifica y no lo ejecuta antes de necesitarlo.
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
